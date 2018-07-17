2b5cbf44d556b5eb1aec0060e7ca4dad155f1d3c
Jogeonsang <rhcn258@gmail.com>
2018-07-17 12:32:51 +0900

    기능 구현

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import App from './containers/App';
 import './index.css';
 
+// Redux 불러오기
+import { createStore } from 'redux';
+import reducers from './reducers';
+import {Provider} from 'react-redux';
+// 스토어 생성
+const store = createStore(reducers);
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
